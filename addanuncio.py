230fa9eeb73901700e57f13c911f8c2808d25487
Matias <lit.io30303@gmail.com>
2025-07-16 16:04:08 -0400

    fix

--- addanuncio.py
+++ addanuncio.py
@@ -20,6 +20,34 @@ TOKEN_SCOPE = "token_addanuncio.json"
 """
 Conseguirme las id de los courses que tiene que subir 
 """
+#cbravo
+# Name: 4°A Lenguaje y Comunicación, ID: 751897652162
+# Name: 4°B Lenguaje y Comunicación, ID: 751897290054
+# Name: 5°A Lenguaje y Comunicación, ID: 751898765349
+# Name: 5°B Lenguaje y Comunicación, ID: 751898649423
+# Name: 5°B Orientación/Tutoría, ID: 751899058480
+# Name: 5°A Proyecto de Innovación, ID: 751898942407
+# Name: 5°B Proyecto de Innovación, ID: 751899032723
+# Name: 6°A Proyecto de Innovación, ID: 751898701460
+# Name: 6°B Proyecto de Innovación, ID: 751897961023
+
+R_CUARTO_A = "751897652162"
+R_CUARTO_B= "751897290054"
+
+R_QUINTO_A = "751898765349"
+R_QUINTO_A_PROYECTO = "751898942407"
+
+R_QUINTO_B="751898649423"
+R_QUINTO_B_ORIENTACION="751899058480"
+R_QUINTO_B_PROYECTO="751899032723"
+
+R_SEXTO_A="751898701460"
+R_SEXTO_B="751897961023"
+
+R_ID_COURSES = [R_CUARTO_A,R_CUARTO_B,R_QUINTO_A,R_QUINTO_A_PROYECTO,R_QUINTO_B,R_QUINTO_B_ORIENTACION,R_QUINTO_B_PROYECTO,R_SEXTO_A,R_SEXTO_B]
+
+#-----------------------------
+#TEST
 # Name: SextoB, ID: 789412787024
 # Name: SextoA, ID: 789412726197
 # Name: QuintoB, ID: 789412718429
@@ -27,12 +55,15 @@ Conseguirme las id de los courses que tiene que subir
 # Name: CuartoB, ID: 782388106581
 # Name: CuartoA, ID: 700371563603
 
+#TEST
 CUARTO_A = "700371563603"
 CUARTO_B= "782388106581"
 QUINTO_A = "789411510193"
 QUINTO_B="789412718429"
 SEXTO_A="789412726197"
 SEXTO_B="789412787024"
+#---------------------------
+
 
 ID_COURSES = [CUARTO_A,CUARTO_B,QUINTO_A,QUINTO_B,SEXTO_A,SEXTO_B]
 
@@ -49,8 +80,11 @@ links_canva = [
     "https://www.canva.com/design/DAGrMu_ZU6I/nFICmZivipwHKXfFQksaog/edit?utm_content=DAGrMu_ZU6I&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
     "https://www.canva.com/design/DAGrMu_ZU6I/nFICmZivipwHKXfFQksaog/edit?utm_content=DAGrMu_ZU6I&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
 ]
+
 INPUT_LINK_CANVA_4 = str(input("Ingrese link de canva 4 básico: "))
 INPUT_LINK_CANVA_5 = str(input("Ingrese link de canva 5 básico: "))
+INPUT_LINK_CANVA_5_PROYECTO = str(input("Ingrese link de canva 5 básico proyecto: "))
+INPUT_LINK_CANVA_5_ORIENTACION = str(input("Ingrese link de canva 5 básico orientación: "))
 INPUT_LINK_CANVA_6 = str(input("Ingrese link de canva 6 básico: "))
 
 
@@ -63,6 +97,91 @@ bot_texts = [
     asyncio.run(teacher_bot())
 ]
 
+#REAL
+#R_CUARTO_A,R_CUARTO_B,R_QUINTO_A,R_QUINTO_A_PROYECTO,R_QUINTO_B,R_QUINTO_B_ORIENTACION,R_QUINTO_B_PROYECTO,R_SEXTO_A,R_SEXTO_B
+
+r_bot_texts = [
+    asyncio.run(teacher_bot()),
+    asyncio.run(teacher_bot()),
+    asyncio.run(teacher_bot()),
+    asyncio.run(teacher_bot()),
+    asyncio.run(teacher_bot()),
+    asyncio.run(teacher_bot()),
+    asyncio.run(teacher_bot()),
+    asyncio.run(teacher_bot()),
+    asyncio.run(teacher_bot())
+]
+
+#ORDEN DE LAS PUBLICACIONES
+r_announcements =[
+    #CUARTO A
+    create_announcement(
+        text=r_bot_texts[0],
+        add_link=True,
+        link_url=INPUT_LINK_CANVA_4,
+        link_title="Presentación Lenguaje"
+    ),
+    #CUARTO B
+    create_announcement(
+        text=r_bot_texts[1],
+        add_link=True,
+        link_url=INPUT_LINK_CANVA_4,
+        link_title="Presentación Lenguaje"
+    ),
+    #QUINTO A
+    create_announcement(
+        text=r_bot_texts[2],
+        add_link=True,
+        link_url=INPUT_LINK_CANVA_5,
+        link_title="Presentación Lenguaje"
+    ),
+    #QUINTO A PROYECTO
+    create_announcement(
+        text=r_bot_texts[3],
+        add_link=True,
+        link_url=INPUT_LINK_CANVA_5_PROYECTO,
+        link_title="Presentación Lenguaje"
+    ),
+    #QUINTO B
+    create_announcement(
+        text=r_bot_texts[4],
+        add_link=True,
+        link_url=INPUT_LINK_CANVA_5,
+        link_title="Presentación Lenguaje"
+    ),
+    #QUINTO B ORIENTACION
+    create_announcement(
+        text=r_bot_texts[5],
+        add_link=True,
+        link_url=INPUT_LINK_CANVA_5_ORIENTACION,
+        link_title="Presentación Lenguaje"
+    ),
+    #QUINTO B PROYECTO
+    create_announcement(
+        text=r_bot_texts[6],
+        add_link=True,
+        link_url=INPUT_LINK_CANVA_5_PROYECTO,
+        link_title="Presentación Lenguaje"
+    ),
+    #SEXTO A
+    create_announcement(
+        text=r_bot_texts[7],
+        add_link=True,
+        link_url=INPUT_LINK_CANVA_6,
+        link_title="Presentación Lenguaje"
+    ),
+    #SEXTO B
+    create_announcement(
+        text=r_bot_texts[8],
+        add_link=True,
+        link_url=INPUT_LINK_CANVA_6,
+        link_title="Presentación Lenguaje"
+    ),
+]
+
+
+#--------
+#TEST
 announcements = [
     create_announcement(
         text=bot_texts[0],
@@ -101,6 +220,7 @@ announcements = [
         link_title="Presentación Lenguaje"
     )
 ]
+
 
 
 
@@ -149,4 +269,8 @@ def main():
 
 
 if __name__ == "__main__":
+  print("-"*80)
+  print("INGRESO DE LINKS DE CANVA")
   main()
+  print("Proceso finalizado")
+  print("-"*80)
