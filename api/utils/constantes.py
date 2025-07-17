e75d0a29ef7a23e23378adf3ba62149bec67c9fc
Matias <lit.io30303@gmail.com>
2025-07-17 15:35:50 -0400

    #fix constante

--- api/utils/constantes.py
+++ api/utils/constantes.py
@@ -1,3 +1,5 @@
 
+#Vercel funciona con /tmp y en local con tmp
 
-TOKEN_ANUNCIO = "tmp/token_addanuncio.json"
+TOKEN_ANUNCIO = "/tmp/token_addanuncio.json"
+TOKEN_ANUNCIO_LOCAL = "tmp/token_addanuncio.json"
